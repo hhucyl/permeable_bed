87e36f58ac64ec8f3c9fe7eafea77274b7f63fdd
hhucyl <hhucyl@hotmail.com>
2018-07-06 22:45:31 +1000

    add bash

--- test.bash
+++ test.bash
@@ -2,3 +2,10 @@
 make test_cd_bb
 echo "collide type, bbtype, h, tau, Nproc"
 ./test_cd_bb 1 1 100 0.8 12
+./test_cd_bb 1 2 100 0.8 12
+./test_cd_bb 1 3 100 0.8 12
+./test_cd_bb 0 1 100 0.8 12
+./test_cd_bb 0 2 100 0.8 12
+./test_cd_bb 0 3 100 0.8 12
+./test_cd_bb 0 0 100 0.8 12
+./test_cd_bb 1 0 100 0.8 12
